ed0367f1938f085c7664f141239622a99398e59e
abhinha-ovhal <133642974+abhinha-ovhal@users.noreply.github.com>
2023-09-19 19:23:01 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+
+function adduser(){
+    player_1 = document.getElementById("player1_name_input").value;
+    player_2 = document.getElementById("player2_name_input").value;
+    localStorage.setItem("Player 2", player_2);
+    localStorage.setItem("Player 1", player_1);
+    window.location.replace("quiz_game_page.html");
+}
